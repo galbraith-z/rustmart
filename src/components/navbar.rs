741184c2def08e20a12db50a9980b3ec771c5e24
Zachary Galbraith <galbraith.z@yahoo.com>
2023-04-25 09:11:49 -0600

    refresh button leads to 404 on hosted site attempted fix

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -39,7 +39,7 @@ impl Component for Navbar {
                 <div class="navbar_title">{"RUSTMART Mtg"}</div>
                 <div class="navbar_cart_value">
                     {format!("${:.2}", cart_value)}
-                    <button class="clear_cart_button" onClick="window.location.reload();">
+                    <button class="clear_cart_button" onClick="location.href=location.href">
                         <i class="fa-solid fa-arrow-rotate-right"></i>
                     </button>
                 </div>
