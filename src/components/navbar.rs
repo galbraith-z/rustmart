8eb24078ded5677cd971b7b078936e51b5baeea1
Zachary Galbraith <galbraith.z@yahoo.com>
2023-04-25 09:03:48 -0600

    trying another reload button implementation

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -39,7 +39,7 @@ impl Component for Navbar {
                 <div class="navbar_title">{"RUSTMART Mtg"}</div>
                 <div class="navbar_cart_value">
                     {format!("${:.2}", cart_value)}
-                    <button class="clear_cart_button" onClick="history.go.(0);">
+                    <button class="clear_cart_button" onClick="history.location.reload();">
                         <i class="fa-solid fa-arrow-rotate-right"></i>
                     </button>
                 </div>
